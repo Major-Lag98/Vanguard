5539879a68fd02d6f5e9b86c8d524e68e9a8a470
Alden <aldensmith06@Gmail.com>
2020-08-15 00:49:56 -0700

    Handmade Grid in which you can set and get values with mouse

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,88 @@
+﻿using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    int _width;
+    int _height;
+    float _cellSize;
+    Vector3 _originPosition;
+    int[,] gridArray;
+    TextMesh[,] debugTextArray;
+
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this._width = width;
+        this._height = height;
+        this._cellSize = cellSize;
+        this._originPosition = originPosition;
+
+        gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        for (int  x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetworldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 5, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetworldPosition(x, y), GetworldPosition(x, y + 1), Color.red, 100f);
+                Debug.DrawLine(GetworldPosition(x, y), GetworldPosition(x + 1, y), Color.red, 100f);
+            }
+        }
+        Debug.DrawLine(GetworldPosition(0, height), GetworldPosition(width,height), Color.red, 100f);
+        Debug.DrawLine(GetworldPosition(width, 0), GetworldPosition(width, height), Color.red, 100f);
+
+
+        SetValue(2, 1, 56);
+    }
+
+    Vector3 GetworldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * _cellSize + _originPosition;
+    }
+
+    void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - _originPosition).x/ _cellSize);
+        y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if(x >= 0 && y >= 0 && x < _width && y < _height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+        
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < _width && y < _height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+
+    Grid grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //x cell count, y cell count, cell size, origin position
+        grid = new Grid(29, 22, 1, new Vector3(-24, -11));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           grid.SetValue(UtilsClass.GetMouseWorldPosition(), 10);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+
+    }
+}
