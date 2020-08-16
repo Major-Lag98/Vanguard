10127748799d795ad6d4893672265b7605856f85
Alden <aldensmith06@Gmail.com>
2020-08-15 19:04:50 -0700

    Placeable towers

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -8,11 +8,23 @@ public class Testing : MonoBehaviour
 
     Grid grid;
 
+    [SerializeField]
+    GameObject tower;
+
+    [SerializeField]
+    Vector2 WH = new Vector2(2,2);
+
+
+    [SerializeField]
+    Vector2 Origin = Vector2.zero;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         //x cell count, y cell count, cell size, origin position
-        grid = new Grid(29, 22, 1, new Vector3(-24, -11));
+        grid = new Grid((int)WH.x, (int)WH.y, 1, Origin);
     }
 
     private void Update()
@@ -27,5 +39,24 @@ public class Testing : MonoBehaviour
             Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
         }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnTower();
+        }
+
+    }
+
+    void SpawnTower()
+    {
+        Vector3 spawnPosition = UtilsClass.GetMouseWorldPosition();
+        spawnPosition = ValidateWorldGridPosition(spawnPosition);
+        spawnPosition += new Vector3(1, 1, 0) * grid.GetCellSize() * .5f;
+        Instantiate(tower, spawnPosition, Quaternion.identity);
+    }
+
+    private Vector3 ValidateWorldGridPosition(Vector3 position)
+    {
+        grid.GetXY(position, out int x, out int y);
+        return grid.GetWorldPosition(x, y);
     }
 }
