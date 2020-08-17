50227f1c7ef044f5aed6152f44af283ed925641b
Brad <bbenton91@gmail.com>
2020-08-17 04:39:25 -0400

    Added back some code I accidentally deleted

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -9,19 +9,35 @@ using CodeMonkey.Utils;
 public class Testing : MonoBehaviour
 {
     [SerializeField]
-    [HideInInspector]
     Grid grid;
-
     
     [SerializeField]
     GameObject tower;
-
 
     // Start is called before the first frame update
     void Start()
     {
         //x cell count, y cell count, cell size, origin position
         //grid = new Grid(29, 22, 1, new Vector3(-24, -11));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 10);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnTower();
+        }
+
     }
 
     private void SpawnTower()
