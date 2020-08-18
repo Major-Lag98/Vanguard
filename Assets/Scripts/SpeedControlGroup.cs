e066e7cad4f457a2ce160286f715c63c757d620d
Brad <bbenton91@gmail.com>
2020-08-18 03:28:20 -0400

    Added a simple control group script for the speed buttons

---
+++ Assets/Scripts/SpeedControlGroup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedControlGroup : MonoBehaviour
+{
+
+    Button[] buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons = GetComponentsInChildren<Button>();
+        foreach (var button in buttons)
+        {
+            button.onClick.AddListener(() => { ClearButtons(); SetSelectedButton(button); });
+        }
+        
+    }
+
+    void ClearButtons()
+    {
+        foreach (var button in buttons)
+        {
+            button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.white;
+        }
+    }
+
+    void SetSelectedButton(Button button)
+    {
+        button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.yellow;
+    }
+}
