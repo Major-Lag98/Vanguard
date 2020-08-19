4f0f9ed7c2b5fd38725110795d61b95e7f5a3cdc
Brad <bbenton91@gmail.com>
2020-08-18 23:44:17 -0400

    Synced the tower with the time ticker

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -12,31 +12,32 @@ public class Tower : MonoBehaviour
     [SerializeField]
     float range = 1;
 
+    bool fired;
+
     private void Start()
     {
+        TimeTicker.Instance.AddOnTimeTickDelegate(Fire, 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    Fire();
-        //}
-        Fire();
-    }
 
-    public void Fire()
+    public void Fire(int time)
     {
+        if (time % 2 == 0 || fired) // We don't want even ticks
+        {
+            fired = false;
+            return;
+        }
         
         var enemy = EnemyManager.Instance.GetClosestEnemy(transform.position, range);
 
         if (!enemy)
             return;
 
-         GameObject projectile = Instantiate(Arrow, transform.position, Quaternion.identity);
+        GameObject projectile = Instantiate(Arrow, transform.position, Quaternion.identity);
         var arrow = projectile.GetComponent<Arrow>();
         arrow.targetPosition = enemy.transform.position;
+
+        fired = true;
     }
 
 }
