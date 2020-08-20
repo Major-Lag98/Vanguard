17719fbce2f2d97c5c20d4e043230426c009da24
Brad <bbenton91@gmail.com>
2020-08-20 01:21:27 -0400

    Forgot to remove this class reference

--- Assets/Scripts/PlacingController.cs
+++ Assets/Scripts/PlacingController.cs
@@ -16,9 +16,6 @@ public class PlacingController : MonoBehaviour
     TextAsset PrefabFile;
 
     [SerializeField]
-    GameObjectPair[] GameObjectList;
-
-    [SerializeField]
     Grid grid;
     
     [SerializeField]
