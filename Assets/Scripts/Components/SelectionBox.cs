0b00cb6ffdd87bc5e733d9af1556a44c3c8687ad
Brad <bbenton91@gmail.com>
2020-08-21 01:30:35 -0400

    More functionality

--- Assets/Scripts/Components/SelectionBox.cs
+++ Assets/Scripts/Components/SelectionBox.cs
@@ -8,17 +8,34 @@ public class SelectionBox : MonoBehaviour
 
     public float EndScale = 1.5f;
     public float TimeToScale = 1f;
+    public bool DoX = true;
+    public bool DoY = false;
+
     // Start is called before the first frame update
     void Start()
     {
         foreach(Transform child in transform)
         {
+            if(DoX && DoY)
+            {
+                Sequence seq = DOTween.Sequence();
+                seq.Append(child.DOScale(EndScale, TimeToScale).SetEase(Ease.InOutSine));
+                seq.Append(child.DOScale(1f, TimeToScale).SetEase(Ease.InOutSine));
+                seq.SetAutoKill(false);
+                seq.SetLoops(-1);
+            }
             //child.DOScaleX(EndScale, 1f)
-            Sequence seq = DOTween.Sequence();
-            seq.Append(child.DOScaleX(EndScale, TimeToScale).SetEase(Ease.InOutSine));
-            seq.Append(child.DOScaleX(1f, TimeToScale).SetEase(Ease.InOutSine));
-            seq.SetAutoKill(false);
-            seq.SetLoops(-1);
+            else if (DoX)
+            {
+                Sequence seq = DOTween.Sequence();
+                seq.Append(child.DOScaleX(EndScale, TimeToScale).SetEase(Ease.InOutSine));
+                seq.Append(child.DOScaleX(1f, TimeToScale).SetEase(Ease.InOutSine));
+                seq.SetAutoKill(false);
+                seq.SetLoops(-1);
+            }else if (DoY)
+            {
+
+            }
         }
     }
 
