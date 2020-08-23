57798af37f4f3058add6c92f89865c3e16e8f02b
Brad <bbenton91@gmail.com>
2020-08-23 01:11:53 -0400

    Added Upgrade object for storing upgrade data

---
+++ Assets/Scripts/Util/Upgrade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Upgrade
+{
+    public int Cost;
+    public int AttackDamage;
+    public int ExtraAttacks;
+    public int ExtraTargets;
+}
