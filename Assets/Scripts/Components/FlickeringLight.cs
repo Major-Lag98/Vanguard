8a795f01085cb06a9cd2b18b31c27baedf008039
Brad <bbenton91@gmail.com>
2020-08-23 01:14:20 -0400

    Removed run in editor

--- Assets/Scripts/Components/FlickeringLight.cs
+++ Assets/Scripts/Components/FlickeringLight.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
-[ExecuteAlways]
 public class FlickeringLight : MonoBehaviour
 {
     [Range(0,1)]
